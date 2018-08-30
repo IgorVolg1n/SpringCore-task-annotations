d4783d67e863203607e46fc7902ea64ebbacba60
Igor Volgin <igor_volgin@epam.com>
2018-08-30 12:58:20 +0300

    Attempting to create "knight" bean

--- src/main/java/com/andreitop/xml/config/AppConfig.java
+++ src/main/java/com/andreitop/xml/config/AppConfig.java
@@ -1,4 +1,9 @@
 package com.andreitop.xml.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.andreitop.xml")
 public class AppConfig {
 }
